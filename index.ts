1032b2f4c5572fc4d910a1098103911eb37c7fba
Lars Erik Storbukås <larserik.storbukas@gmail.com>
2021-02-18 22:45:37 +0100

    Added Express / Node.js wep application for serving SVG badges

---
+++ index.ts
@@ -0,0 +1,35 @@
+import express = require('express');
+import fs = require('fs');
+
+const PORT = Number(process.env.PORT) || 80;
+const app = express();
+
+app.get(['/:kjennemerke', '/', '*'], (req, res) => {
+  let kjennemerke = req.params.kjennemerke;
+
+  fs.readFile('personlig-kjennemerke.svg', 'utf-8', function (err, data) {
+    if (err) {
+      res.send("An error occurred.");
+    } else {
+      // set the content type based on the file
+      res.setHeader('Content-Type', 'image/svg+xml');
+
+      if (kjennemerke) {
+        if (kjennemerke.length > 7) {
+          kjennemerke = kjennemerke.substring(0, 7);
+        }
+
+        res.send(data.replace('GEEK', kjennemerke.toUpperCase()));
+      } else {
+        res.send(data);
+      }
+    }
+    res.end();
+  });
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
+module.exports = server;
